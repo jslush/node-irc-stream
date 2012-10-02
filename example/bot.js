003e47020d87b3502db87e11045bae43a8279ecb
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2012-10-02 15:23:45 +0300

    Improve logger middleware.

--- example/bot.js
+++ example/bot.js
@@ -5,14 +5,12 @@ var irc = require('..');
 var client = new irc()
   .set('nick', 'streamBot1337')
   .set('address', 'irc.freenode.org')
-  .input('log', {colors: true, prefix: '<< '})
-  .output('log', {colors: true, prefix: '>> '})
-  .input('floodprotection', {interval: 2000, maxFlood: 3})
+  .use('logger')
   .use('exception-handler')
+  .input('floodprotection', {interval: 2000, maxFlood: 3})
   .use('message-stream')
   .connect(function () {
     client.join('#jslush', function (ch) {
-      console.log('JOINED', ch);
       ch.write('hi!');
       // Pipe ALL THE THINGS!
       process.stdin.pipe(ch)
