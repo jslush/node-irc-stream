f5b199af2f84a839d36b5743c76c7c290618d738
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2012-10-02 15:51:09 +0300

    Message Stream now emits an object not a string.

--- example/bot.js
+++ example/bot.js
@@ -1,6 +1,17 @@
 'use strict';
 
-var irc = require('..');
+var irc = require('..')
+  , es = require('event-stream')
+  , format = require('util').format;
+
+function f(i) {
+  return '' + ((i < 10 ? ' ' + i : i));
+}
+
+var formatter = es.mapSync(function (o) {
+  return format('%s%s (%s) <%s> %s\n',
+    f(o.timestamp.getHours()), f(o.timestamp.getMinutes()), o.to, o.nick, o.text);
+});
 
 var client = new irc()
   .set('nick', 'streamBot1337')
@@ -14,6 +25,21 @@ var client = new irc()
       ch.write('hi!');
       // Pipe ALL THE THINGS!
       process.stdin.pipe(ch)
+        .pipe(formatter)
         .pipe(process.stdout);
     });
   });
+
+/* You can also use events instead of callbacks:
+client.on('connected', function () {
+  client.join('#jslush');
+
+  client.on('joined#jslush', function (ch) {
+    ch.write('hi!');
+
+    process.stdin.pipe(ch)
+      .pipe(formatter)
+      .pipe(process.stdout);
+  });
+});
+*/
