d69772cbc93252e2b4025d8486d3ca578c830b25
Vesa Laakso <laakso.vesa@gmail.com>
2012-10-07 22:01:23 +0300

    Make the testing work again for middleware loader tests

--- test/LoadMiddlewareTest.js
+++ test/LoadMiddlewareTest.js
@@ -1,19 +1,20 @@
 'use strict';
 
-var IRC = require('..')
+var vows = require('vows')
+  , IRC = require('..')
   , assert = require('assert');
 
-module.exports = {
+vows.describe('Testing middleware loading').addBatch({
   'An IRC Client': {
     topic: new IRC(),
     'Can load CTCP middleware': function (client) {
-      assert.doesNotThrow(function () { client.use('ctcp') });
+      assert.doesNotThrow(function () { client.use('ctcp'); });
     },
     'Cannot load middleware with directory separators': function (client) {
-      assert.throws(function () { client.use('../middleware/ctcp') });
+      assert.throws(function () { client.use('../middleware/ctcp'); });
     },
     'Cannot load unexisting middlewares': function (client) {
-      assert.throws(function () { client.use('unexisting-middleware') });
+      assert.throws(function () { client.use('unexisting-middleware'); });
     }
   }
-};
+}).exportTo(module);
