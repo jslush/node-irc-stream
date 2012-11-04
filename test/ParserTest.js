de8e84cced58fd2a24f1700cfb84f8c1bbc9c6a3
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2012-11-04 19:17:47 +0200

    Rewrote ParserTest, neat!

--- test/ParserTest.js
+++ test/ParserTest.js
@@ -1,52 +1,57 @@
 'use strict';
 
 var vows = require('vows')
-  , parser = require('../lib/middleware/parser')
+  , Parser = require('../lib/middleware/parser')
   , assert = require('assert')
   , crypto = require('crypto');
 
-vows.describe('Parser Test').addBatch({
-  'A Parser': {
-    topic: parser,
-    'parsing 001 message': {
-      topic: function (parser) {
-        var cb = this.callback;
-        parser.once('data', function (data) {
-          cb(null, data);
-        });
-        parser.write(':lindbohm.freenode.net 001 streamBot1337 :Welcome to the freenode Internet Relay Chat Network streamBot1337\r\n');
-      },
-      'parses 001 message': function (data) {
-        assert.deepEqual(data, {
-          prefix: 'lindbohm.freenode.net',
-          server: 'lindbohm.freenode.net',
-          command: '001',
-          rawCommand: '001',
-          commandType: 'normal',
-          args: [ 'streamBot1337',
-                  'Welcome to the freenode Internet Relay Chat Network streamBot1337' ]
-        });
+// Load messages to test the parser with.
+var messages = require('./messages');
+
+// Define a macro for creating a parsing test.
+function parseMessage(msg) {
+  var parser = Parser();
+  return function () {
+    parser.once('data', this.callback.bind(null, null));
+    parser.write(msg + '\r\n');
+  };
+}
+
+// This functions turns the json into a test suite.
+function parseMessages() {
+  var suite = {};
+  Object.keys(messages).forEach(function (msg) {
+    suite['parsing ' + msg] = {
+      topic: parseMessage(messages[msg][0]),
+      'Parsed correctly': function (data) {
+        assert.deepEqual(data, messages[msg][1]);
       }
-    },
+    };
+  });
+  return suite;
+}
+
+vows.describe('Parser Test')
+  // Tests each message in the messages object.
+  .addBatch({ 'A parser': parseMessages() })
+  // Tests fuzzy random data for exceptions.
+  .addBatch({ 'A parser': {
     'parsing random message': {
       topic: function (parser) {
-        return function () {
-          for (var i = 999; i >= 0; i--) {
-            try {
-              parser.write(crypto.randomBytes(256).toString() + '\r\n');
-            } catch (e) {
-              // We only want TypeErrors, so anything else will fail.
-              if (!(e instanceof TypeError)) {
-                return e;
-              }
-            }
+        for (var i = 999; i >= 0; i--) {
+          try { parser.write(crypto.randomBytes(256).toString() + '\r\n'); }
+          catch (e) { // We only want TypeErrors, so anything else will fail.
+            if (!(e instanceof TypeError)) { return e; }
           }
-          return true;
-        };
+        }
+        return true;
       },
-      'crashes cleanly': function (f) {
-        assert.isTrue(f());
+      'Crashes cleanly': function (clean) {
+        assert.isTrue(clean);
       }
     }
-  }
-}).exportTo(module);
+  }})
+.exportTo(module);
+
+
+      
